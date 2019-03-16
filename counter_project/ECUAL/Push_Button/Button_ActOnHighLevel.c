2811d544e8e8027ffb735fdc833b877378fabeea
Mohned-Mohamed <mohnedmohamed.98@hotmail.com>
2019-03-17 00:06:55 +0200

    add Button_ActOnHighLevel.c

---
+++ counter_project/ECUAL/Push_Button/Button_ActOnHighLevel.c
@@ -0,0 +1,27 @@
+#include "button.h"
+
+/**
+  * @brief This function used to take action while button is on high level state.
+  * @param port_index specifies the number of port that the button is on,
+	* @param pin_Number specifies pin number of the button,
+	* @param pAction is a pointer to function that contains action to be done
+  * @retval None
+  */
+	
+void Button_ActOnHighLevel( uint8 port_index, uint8 pin_Number, void (*pAction)(void) )
+{
+	if(DIO_ReadPort(port_index,1<<pin_Number))
+	{
+		SysTick_delay_ms(30);
+		if(DIO_ReadPort(port_index,1<<pin_Number))
+		{
+			while(DIO_ReadPort(port_index,1<<pin_Number))
+			{
+				pAction();
+				SysTick_delay_ms(200);
+			}
+		}
+	}
+	
+	
+}
