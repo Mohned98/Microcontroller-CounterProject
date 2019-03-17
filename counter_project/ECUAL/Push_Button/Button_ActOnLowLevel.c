14df42bf401324fb5336d41d54fc0c99f79b107d
monaa12 <36414725+monaa12@users.noreply.github.com>
2019-03-18 00:29:25 +0300

    Update Button_ActOnLowLevel.c

--- counter_project/ECUAL/Push_Button/Button_ActOnLowLevel.c
+++ counter_project/ECUAL/Push_Button/Button_ActOnLowLevel.c
@@ -1,4 +1,11 @@
 #include "button.h"
+/**
+  * @brief This function used to take action while button is on low level state.
+  * @param port_index specifies the number of port that the button is on,
+	* @param pin_Number specifies pin number of the button,
+	* @param pAction is a pointer to function that contains action to be done
+  * @retval None
+  */
 void Button_ActOnLowLevel( uint8 port_index, uint8 pin_Number, void (*pAction)(void) )
 {
 if(DIO_ReadPort(port_index,~(1<<pin_Number) ))
