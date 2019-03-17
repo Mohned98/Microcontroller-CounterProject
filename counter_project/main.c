7d8be8df7fdabcba3db62e2e7534fe2a057a8920
Mohand-Gamal <mohandgamal@hotmail.com>
2019-03-17 18:59:58 +0200

    Decrement, Increment and Reset Functions

--- counter_project/main.c
+++ counter_project/main.c
@@ -1,12 +1,35 @@
 #include "MACROS.h"
 #include "STANDARD_TYPES.h"
+uint32 count=0;
+
+void Increment(void){
+	if(count==999)
+		count=0;
+	else
+		count++;
+	LCD_intgerToString(count);
+}
+
+void Decrement(void){
+	if(count==0)
+		count=999;
+	else
+		count--;
+	LCD_intgerToString(count);
+}
+
+void Reset(void){
+	count=0;
+	LCD_intgerToString(count);
+}
 
 void SystemInit(void){}
+	
 int main(void)
 {
 		while(1) 
 		{
-			
+			 
 		}
 	
 }
