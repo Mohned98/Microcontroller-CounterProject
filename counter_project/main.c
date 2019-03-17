af4de33a67d5588cb757aa3e9aae04638f747bff
Mohand-Gamal <mohandgamal@hotmail.com>
2019-03-17 19:11:34 +0200

    Adding Comments and functions Prototypes (Increment,Decrement,Reset)

--- counter_project/main.c
+++ counter_project/main.c
@@ -2,26 +2,9 @@
 #include "STANDARD_TYPES.h"
 uint32 count=0;
 
-void Increment(void){
-	if(count==999)
-		count=0;
-	else
-		count++;
-	LCD_intgerToString(count);
-}
-
-void Decrement(void){
-	if(count==0)
-		count=999;
-	else
-		count--;
-	LCD_intgerToString(count);
-}
-
-void Reset(void){
-	count=0;
-	LCD_intgerToString(count);
-}
+void Increment(void); /*Incrementation of the counter function*/
+void Decrement(void); /*Decrementation of the counter function*/
+void Reset(void); /*Reset of the counter function*/
 
 void SystemInit(void){}
 	
@@ -34,3 +17,26 @@ int main(void)
 	
 }
 
+/*Reset of the counter function*/
+void Reset(void){
+	count=0;
+	LCD_intgerToString(count); /*display on LCD*/
+}
+
+/*Incrementation of the counter function*/
+void Increment(void){
+	if(count==999) /*Reset the counter when reaching 999*/
+		Reset();
+	else
+		count++;
+	LCD_intgerToString(count); /*display on LCD*/
+}
+
+/*Decrementation of the counter function*/
+void Decrement(void){
+	if(count==0) /*Return to 999 when reaching 0*/
+		count=999;
+	else
+		count--;
+	LCD_intgerToString(count); /*display on LCD*/
+}
