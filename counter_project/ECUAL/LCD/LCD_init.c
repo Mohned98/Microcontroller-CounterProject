247d283ec9bc7d7b35d7676267abf8aca17b502d
menna-hussien <31513435+menna-hussien@users.noreply.github.com>
2019-03-20 01:06:14 +0200

    initializes the LCD

--- counter_project/ECUAL/LCD/LCD_init.c
+++ counter_project/ECUAL/LCD/LCD_init.c
@@ -12,24 +12,17 @@ void LCD_init(void){
 	Port_Init(LCD_CTRL_PORT); //port A 
 	Port_SetPinDirection(LCD_CTRL_PORT, 1<<RS, PORT_PIN_OUT );
 	Port_SetPinDirection(LCD_CTRL_PORT, 1<<RW, PORT_PIN_OUT );
-	SysTick_delay_ms(1);
 	Port_SetPinDirection(LCD_CTRL_PORT, 1<<E, PORT_PIN_OUT );
 
 	Port_Init(LCD_DATA_PORT); // port B 
-	Port_SetPinDirection(LCD_DATA_PORT, 1<<RS, PORT_PIN_OUT );
-	Port_SetPinDirection(LCD_DATA_PORT, 1<<RW, PORT_PIN_OUT );
-	SysTick_delay_ms(1);
-	Port_SetPinDirection(LCD_DATA_PORT, 1<<E, PORT_PIN_OUT );
+	Port_SetPinDirection(LCD_DATA_PORT, 0xFF, PORT_PIN_OUT );
 	
 	
-	LCD_sendCommand(WAKEUP_COMMAND);                //wakeup command
-	//delay();
 	LCD_sendCommand(TWO_LINE_LCD_Eight_BIT_MODE);   //8-bits data 
-	//delay();
 	LCD_sendCommand(SET_CURSOR_LOCATION);           // start from the beginning of the first line
-	//delay();
-	LCD_sendCommand(CURSOR_BLINKING);               //cursor blinking command
-	//delay();
-	
+	LCD_sendCommand(CURSOR_OFF);                		//cursor blinking command
+	LCD_sendCommand(CLEAR_COMMAND);                 //clear command
 	
 }
+
+
